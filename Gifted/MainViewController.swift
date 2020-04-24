a03d9c9667eb7befc5687f1805edf2b019ebbc78
Nick <ptnguyen1901@gmail.com>
2020-04-24 16:32:06 -0400

    Add app icon, UI setup

---
+++ Gifted/MainViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  Gifted
+//
+//  Created by Nick Nguyen on 4/24/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    
+    @IBOutlet weak var menuCollectionView: UICollectionView! {
+        didSet {
+            menuCollectionView.delegate = self
+            menuCollectionView.dataSource = self
+        }
+    }
+    var menuItem = ["Photo to Gif","Video to Gif","AR","Camera","Gif Editor","Timelapse","Slowmotion","Live Photo to GIF"]
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.isHidden = true
+    }
+
+
+}
+
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return menuItem.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let menu = menuItem[indexPath.row]
+        let cell =  collectionView.dequeueReusableCell(withReuseIdentifier: "MenuCell", for: indexPath) as! MenuCell
+        cell.menuLabel.text = menu
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        var totalUsableWidth = collectionView.frame.width
+        let inset = self.collectionView(collectionView, layout: collectionViewLayout, insetForSectionAt: indexPath.section)
+        totalUsableWidth -= inset.left + inset.right
+        
+        let minWidth: CGFloat = 150.0
+        let numberOfItemsInOneRow = Int(totalUsableWidth / minWidth)
+        totalUsableWidth -= CGFloat(numberOfItemsInOneRow - 1) * flowLayout.minimumInteritemSpacing
+        let width = totalUsableWidth / CGFloat(numberOfItemsInOneRow)
+        return CGSize(width: width, height: width)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 20.0, bottom: 0, right: 20.0)
+    }
+}
