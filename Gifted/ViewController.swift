f6e4dd543a8922ffd3af55b19f9a0ba78ff92af0
Nick <ptnguyen1901@gmail.com>
2020-04-24 15:55:03 -0400

    Create Xcode project, add gitignore

---
+++ Gifted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gifted
+//
+//  Created by Nick Nguyen on 4/24/20.
+//  Copyright © 2020 Nick Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
